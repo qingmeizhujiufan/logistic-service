0aafae27a95b564b395bd7cbd69f9ce081e6323b
钟正 <616028858@qq.com>
2018-03-28 09:51:26 +0800

    加入企业相册demo

--- src/modules/pub/enterpriseAlbum/component/enterpriseAlbum.js
+++ src/modules/pub/enterpriseAlbum/component/enterpriseAlbum.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavBar, Icon, List, Radio, InputItem, TextareaItem, Toast, Button, WhiteSpace, WingBlank } from 'antd-mobile';
+import { NavBar, Icon, Carousel, WhiteSpace, WingBlank } from 'antd-mobile';
 import '../enterpriseAlbum.less';
 
 class EnterpriseAlbum extends React.Component {
@@ -7,10 +7,19 @@ class EnterpriseAlbum extends React.Component {
         super(props);
         
         this.state = {
+          data: ['1', '2', '3'],
+          imgHeight: 176,
+          slideIndex: 0,
         };
     }
   
     componentDidMount() {
+      // simulate img loading
+      setTimeout(() => {
+        this.setState({
+          data: ['AiyWuByWklrrUDlFignR', 'TekJlZRVCjLFexlOCuWn', 'IJOtIlfsYdTyaDTRVrLI'],
+        });
+      }, 100);
     }
 
     //返回
@@ -30,11 +39,45 @@ class EnterpriseAlbum extends React.Component {
             >企业相册</NavBar>
             <div className='zui-content index zui-scroll-wrapper'>
               <div className="zui-scroll">
-                  
-              </div>   
-            </div>
-          </div>
-        );
+                  <div className="sub-title">Space</div>
+                  <Carousel className="space-carousel"
+                    frameOverflow="visible"
+                    cellSpacing={10}
+                    slideWidth={0.8}
+                    autoplay
+                    infinite
+                    beforeChange={(from, to) => console.log(`slide from ${from} to ${to}`)}
+                    afterChange={index => this.setState({ slideIndex: index })}
+                  >
+                    {this.state.data.map((val, index) => (
+                      <a
+                        key={val}
+                        href="http://www.alipay.com"
+                        style={{
+                          display: 'block',
+                          position: 'relative',
+                          top: this.state.slideIndex === index ? -10 : 0,
+                          height: this.state.imgHeight,
+                          boxShadow: '2px 1px 1px rgba(0, 0, 0, 0.2)',
+                        }}
+                      >
+                        <img
+                          src={`https://zos.alipayobjects.com/rmsportal/${val}.png`}
+                          alt=""
+                          style={{ width: '100%', verticalAlign: 'top' }}
+                          onLoad={() => {
+                            // fire window resize event to change height
+                            window.dispatchEvent(new Event('resize'));
+                            this.setState({ imgHeight: 'auto' });
+                          }}
+                        />
+                      </a>
+                    ))}
+                  </Carousel>
+                        </div>   
+                      </div>
+                    </div>
+                  );
     }
 }
 
