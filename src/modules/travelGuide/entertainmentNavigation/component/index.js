a88f099abe9aded5db9a6663748a6f58415c5ef8
钟正 <616028858@qq.com>
2018-05-24 20:16:56 +0800

    完善生活导航

--- src/modules/travelGuide/entertainmentNavigation/component/index.js
+++ src/modules/travelGuide/entertainmentNavigation/component/index.js
@@ -1,7 +1,69 @@
 import React from 'react';
-import { NavBar, Icon, Card, WingBlank, WhiteSpace } from 'antd-mobile';
-import {Map, Marker, NavigationControl, InfoWindow, TrafficLayer} from 'react-bmap';
+import { NavBar, Icon, List, WingBlank, WhiteSpace } from 'antd-mobile';
 import '../index.less';
+
+const Item = List.Item;
+const Brief = Item.Brief;
+const getUrl = destination => `http://api.map.baidu.com/direction?origin=latlng:30.543613,114.489567|name:武汉高铁职业技能训练段&destination=${destination}&mode=driving&region=武汉&output=html&src=fxhh`;
+const trafficPoint = [
+    {
+        title: '武汉站',
+        desc: '湖北省武汉市洪山区杨春湖南侧'
+    },{
+        title: '武昌站',
+        desc: '湖北省武汉市武昌区中山路'
+    },{
+        title: '汉口站',
+        desc: '湖北省武汉市江汉区发展大道金家墩1号'
+    },{
+        title: '武汉天河国际机场',
+        desc: '武汉市黄陂区机场大道'
+    },
+];
+
+const school = [
+    {
+        title: '武汉大学',
+        desc: '985工程 211工程 研究生院'
+    },{
+        title: '华中师范大学',
+        desc: '211工程 双一流（世界一流学科建设高校）2011计划 111计划 '
+    },{
+        title: '华中科技大学',
+        desc: '211工程 985工程 七校联合办学 双一流（世界一流大学A类建设高校）教育部直属高校 全国重点大学 2011计划'
+    },{
+        title: '武汉理工大学',
+        desc: '211工程 985平台 111计划'
+    },
+];
+
+const tour = [
+    {
+        title: '光谷广场',
+        desc: '武汉轨道交通2号线1期东南端的首末站'
+    },{
+        title: '楚河汉街',
+        desc: '湖北省武汉市核心地段，武昌区东湖和沙湖之间'
+    },{
+        title: '武汉植物园',
+        desc: '中国湖北省武汉市武昌区磨山'
+    },{
+        title: '武汉动物园',
+        desc: '武汉市汉阳区墨水湖畔'
+    },{
+        title: '磨山',
+        desc: '国家AAAAA级旅游景区'
+    },{
+        title: '武汉东湖风景区',
+        desc: '国家5A级旅游景区'
+    },{
+        title: '东湖绿道',
+        desc: '5A级旅游景区'
+    },{
+        title: '武汉欢乐谷',
+        desc: '东湖生态旅游风景区欢乐大道196号（近武汉火车站）'
+    },
+];
 
 class BusInformation extends React.Component {
     constructor(props) {
@@ -19,8 +81,8 @@ class BusInformation extends React.Component {
     componentDidMount() {
     }
 
-    btnClick = (url) => {
-      this.context.router.push(url);
+    BaiduGuide = (url) => {
+      window.location.href = url;
     }
 
     //返回
@@ -41,7 +103,57 @@ class BusInformation extends React.Component {
           >生活导航</NavBar>
           <div className="zui-content zui-scroll-wrapper">
             <div className="zui-scroll">
-              <p>敬请期待~</p>
+                <List renderHeader={() => '重要交通点'}>
+                    {
+                        trafficPoint.map((item, index) => {
+                            return (
+                                <Item
+                                    key={index}
+                                    arrow="horizontal"
+                                    multipleLine
+                                    onClick={() => this.BaiduGuide(getUrl(item.title))}
+                                    platform="android"
+                                >
+                                    {item.title}<Brief>{item.desc}</Brief>
+                                </Item>
+                            )
+                        })
+                    }
+                </List>
+                <List renderHeader={() => '知名高校'}>
+                    {
+                        school.map((item, index) => {
+                            return (
+                                <Item
+                                    key={index}
+                                    arrow="horizontal"
+                                    multipleLine
+                                    onClick={() => this.BaiduGuide(getUrl(item.title))}
+                                    platform="android"
+                                >
+                                    {item.title}<Brief>{item.desc}</Brief>
+                                </Item>
+                            )
+                        })
+                    }
+                </List>
+                <List renderHeader={() => '旅游景点'}>
+                    {
+                        tour.map((item, index) => {
+                            return (
+                                <Item
+                                    key={index}
+                                    arrow="horizontal"
+                                    multipleLine
+                                    onClick={() => this.BaiduGuide(getUrl(item.title))}
+                                    platform="android"
+                                >
+                                    {item.title}<Brief>{item.desc}</Brief>
+                                </Item>
+                            )
+                        })
+                    }
+                </List>
             </div>   
           </div>
         </div>
