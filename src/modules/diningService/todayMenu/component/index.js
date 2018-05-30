ad4179616b31c9b93654353907b9e71711ed45bf
zhongzhenga <616028858@qq.com>
2018-05-31 00:30:04 +0800

    修改今日菜单；

--- src/modules/diningService/todayMenu/component/index.js
+++ src/modules/diningService/todayMenu/component/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavBar, Icon, Card, WingBlank, WhiteSpace, Flex, Toast } from 'antd-mobile';
+import {NavBar, Icon, Toast} from 'antd-mobile';
 import QueueAnim from 'rc-queue-anim';
 import ajax from 'Utils/ajax';
 import restUrl from 'RestUrl';
@@ -9,122 +9,124 @@ import noData from 'Img/no-data.png';
 const getDailyDishUrl = restUrl.ADDR + 'server/getDailyDish';
 
 const tabs = [
-  {
-    label: '早餐',
-  }, {
-    label: '午餐',
-  }, {
-    label: '晚餐',
-  }
+    {
+        label: '早餐',
+    }, {
+        label: '午餐',
+    }, {
+        label: '晚餐',
+    }
 ];
-
-import todayMenu from 'Img/today-menu.jpg';
 
 class TodayMenu extends React.Component {
     constructor(props) {
         super(props);
-        
+
         this.state = {
-          tabIndex: 1,
-          data: []
+            tabIndex: 1,
+            data: []
         };
     }
-  
+
     componentDidMount() {
-      this.getDailyDish();
+        this.getDailyDish();
     }
 
     getDailyDish = () => {
-      var param = {};
-      param.id = this.props.params.id;
-      ajax.getJSON(getDailyDishUrl, param, (data) => {
-        data = data.backData;
-        this.setState({
-          data
+        Toast.loading('正在加载...', 0);
+        var param = {};
+        param.id = this.props.params.id;
+        ajax.getJSON(getDailyDishUrl, param, (data) => {
+            data = data.backData;
+            this.setState({
+                data
+            });
+            if (data.length === 0) {
+                Toast.info('管理员暂未设置今天菜单', 1);
+            }
+            Toast.hide();
         });
-        if(data.length === 0){
-          Toast.info('管理员暂未设置今天菜单', 1);
-        }
-      });
     }
 
     filterData = (data) => {
-      const {tabIndex} = this.state;
-      const dataSource = data;
-      return dataSource.filter((item) => {
-        return item.dish_type === tabs[tabIndex - 1].label
-      })
+        const {tabIndex} = this.state;
+        const dataSource = data;
+        return dataSource.filter((item) => {
+            return item.dish_type === tabs[tabIndex - 1].label && item.dish_week && item.dish_week.indexOf(new Date().getDay().toString()) > -1
+        })
     }
 
     //返回
     callback = () => {
-      this.context.router.goBack();
-    } 
+        this.context.router.goBack();
+    }
 
     changeTab = (index) => {
-      console.log('index === ', index);
-      this.setState({
-        tabIndex: index
-      });
+        console.log('index === ', index);
+        this.setState({
+            tabIndex: index
+        });
     }
 
     render() {
-        const { tabIndex, data } = this.state;
+        const {tabIndex, data} = this.state;
         const dataSource = this.filterData(data);
         return (
-          <div className="todayMenu">
-            <NavBar
-              mode="light"
-              icon={<Icon type="left" />}
-              leftContent="返回" 
-              onLeftClick={this.callback}
-            >今日菜单</NavBar>
-            <div className='zui-content zui-scroll-wrapper'>
-              <div className="zui-scroll">
-                <div className="tab-button-group">
-                  <QueueAnim delay={500}>
-                    {
-                      tabs.map((item, index) => {
-                        const _class = `tab-button ${tabIndex === (index + 1) ? 'active' : ''}`;
-                        return (
-                          <span key={index} className={_class} onClick={() => this.changeTab(index + 1)}>{item.label}</span>
-                        )
-                      })
-                    }
-                  </QueueAnim>
-                </div>
-                <div>
-                  <ul className="zui-list-unstyled dish-list">
-                    {
-                      dataSource.length > 0 ? dataSource.map((item, index) => {
-                        return (
-                          <li key={index}>
-                            <div>
-                              <div className='wrap-img'>
-                                <img src={restUrl.BASE_HOST + 'UpLoadFile/' + item.dish_img + '.png'} />
-                              </div>
-                              <div className="title">{item.dish_title}</div>
-                              <div className="desc">{item.dish_content}</div>
-                            </div>
-                          </li>
-                        )
-                      }) : (
-                        <div className="wrap-no-data">
-                          <img src={noData} />
+            <div className="todayMenu">
+                <NavBar
+                    mode="light"
+                    icon={<Icon type="left"/>}
+                    leftContent="返回"
+                    onLeftClick={this.callback}
+                >今日菜单</NavBar>
+                <div className='zui-content zui-scroll-wrapper'>
+                    <div className="zui-scroll">
+                        <div className="tab-button-group">
+                            <QueueAnim delay={500}>
+                                {
+                                    tabs.map((item, index) => {
+                                        const _class = `tab-button ${tabIndex === (index + 1) ? 'active' : ''}`;
+                                        return (
+                                            <span key={index} className={_class}
+                                                  onClick={() => this.changeTab(index + 1)}>{item.label}</span>
+                                        )
+                                    })
+                                }
+                            </QueueAnim>
                         </div>
-                      )
-                    }
-                  </ul>
+                        <div>
+                            <ul className="zui-list-unstyled dish-list">
+                                {
+                                    dataSource.length > 0 ? dataSource.map((item, index) => {
+                                        return (
+                                            <li key={index}>
+                                                <div>
+                                                    <div className='wrap-img'>
+                                                        <img
+                                                            src={restUrl.BASE_HOST + 'UpLoadFile/' + item.dish_img + '.png'}/>
+                                                    </div>
+                                                    <div className="title">{item.dish_title}</div>
+                                                    <div className="desc">{item.dish_content}</div>
+                                                </div>
+                                            </li>
+                                        )
+                                    }) : (
+                                        <div className="wrap-no-data">
+                                            <img src={noData}/>
+                                        </div>
+                                    )
+                                }
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-              </div>   
             </div>
-          </div>
         );
     }
 }
 
-TodayMenu.contextTypes = {  
-    router: React.PropTypes.object  
-} 
+TodayMenu.contextTypes = {
+    router: React.PropTypes.object
+}
 
-export default  TodayMenu;
+export default TodayMenu;
